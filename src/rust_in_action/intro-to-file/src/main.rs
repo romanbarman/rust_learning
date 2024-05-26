8065df036100689255d9a7d0b1251d5e3ae61535
Roman Zanko <roman.zanko.vl@gmail.com>
2024-05-26 18:30:52 +0300

    Updated intro to file project

--- src/rust_in_action/intro-to-file/src/main.rs
+++ src/rust_in_action/intro-to-file/src/main.rs
@@ -4,20 +4,10 @@ use std::io::prelude::*;
 
 fn main() {
     let f = File::open("../../../README.md").unwrap();
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
     
-    let mut line = String::new();
-
-    loop {
-        let len = reader.read_line(&mut line)
-            .unwrap();
-        
-        if len == 0 {
-            break
-        }
-        
-        println!("{} ({} bytes long)", line, len);
-        
-        line.truncate(0);
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{} ({} bytes long)", line, line.len());
     }
 }
