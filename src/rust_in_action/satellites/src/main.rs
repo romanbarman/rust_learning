cf3b6fb85837fee1b12f993c5a6142e4205827c2
Roman Zanko <roman.zanko.vl@gmail.com>
2024-06-02 15:47:59 +0300

    Updated check status function in satellites project

--- src/rust_in_action/satellites/src/main.rs
+++ src/rust_in_action/satellites/src/main.rs
@@ -8,8 +8,9 @@ enum StatusMessage {
     Ok
 }
 
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-    StatusMessage::Ok
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
 }
 
 fn main() {
@@ -17,13 +18,11 @@ fn main() {
     let sat_b = CubeSat { id: 1};
     let sat_c = CubeSat { id: 2};
     
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);    
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 }
